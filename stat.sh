375f786c966262f36ddd5dddbb2d22985bad3115
dcvan <dcvan@cs.unc.edu>
2015-11-09 10:54:33 -0500

    stat script

---
+++ stat.sh
@@ -0,0 +1,13 @@
+awk -F',' '{
+  if(NR > 16){
+     tp += $10;
+     spout_tp += $12;
+     lat += $13;
+     max_lat += $14;
+     count ++;
+  }
+}
+END{
+  print tp/count, spout_tp/count, lat/count, max_lat/count;
+}
+' $1
